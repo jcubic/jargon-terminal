8f661d4197095ce3c4767e3e98d98de3e40e5f8b
Jakub Jankiewicz <jcubic@onet.pl>
2025-04-25 00:06:26 +0200

    move JS code into separate file

---
+++ index.js
@@ -0,0 +1,227 @@
+rpc({
+    url: 'https://terminal.jcubic.pl/service.php',
+    promisify: true,
+    error: function(error) {
+        try {
+            error = JSON.parse(error);
+        } catch(e) {}
+        var message;
+        if (error.error) {
+            error = error.error;
+            message = error.message + ' in ' + error.file +
+                '\n[' + error.at + '] ' + error.line;
+        } else {
+            message = error.message || error;
+        }
+        var terminal = $.terminal.active();
+        if (terminal) {
+            terminal.error(message);
+            terminal.resume();
+        } else {
+            alert(message);
+        }
+    }
+}).then(function(service) {
+
+    const commands = {
+        async jargon(...args) {
+            const options = $.terminal.parse_options(args, { boolean: ['s']});
+            // there are options
+            if (options._.length) {
+                const query = options._.join(' ');
+                // search option
+                if (options.s) {
+                    const { data, error } = await jargon_search(query);
+                    if (error) {
+                        this.error(error);
+                    } else {
+                        this.echo(data.map((term) => {
+                            return `<name>${term}</name>`;
+                        }).join('\n'));
+                    }
+                } else {
+                    // normal query
+                    const { data, error } = await jargon_term(query);
+                    if (error) {
+                        this.error(error);
+                    } else {
+                        const entry = format_entry(data);
+                        this.echo(entry.trim(), {
+                            keepWords: true
+                        });
+                    }
+                }
+            } else {
+                const msg = 'This is the Jargon File, a comprehens'+
+                      'ive compendium of hacker slang illuminating m'+
+                      'any aspects of hackish tradition, folklore, a'+
+                      'nd humor.\n\nusage: jargon [-s] &lt;QUERY&gt;'+
+                      '\n\n-s search jargon file';
+                this.echo(msg, { keepWords: true });
+            }
+        },
+        async rfc(...args) {
+            if (args[0] == '--help') {
+                term.echo('Browser of RFC documents, using less unix command.\n\n' +
+                          'If you execute without arguments you will get index page\n' +
+                          'And on that page you can use / followed by text, to search\n' +
+                          'links to RFC documents are clickable');
+            } else {
+                var number = args.length ? args[0] : null;
+                try {
+                    const rfc = await service.rfc(number);
+                    term.less(rfc.replace(/^[\s\n]+|[\s\n]+$/g, ''));
+                } catch (err) {
+                    print_error(err);
+                }
+            }
+        },
+        credits() {
+            return [
+                '',
+                'Tools, libraries, and services used:',
+                '* [[!b;#fff;;;https://terminal.jcubic.pl/]jQuery Terminal]',
+                '* [[!b;#fff;;;https://github.com/patorjk/figlet.js]Figlet.js] + Modular and Rectangles fonts',
+                '* [[!b;#fff;;;http://catb.org/jargon/html/index.html]Jargon File]',
+                '* [[!b;#fff;;;https://www.rfc-editor.org/]RFC Editor]',
+                '* [[!b;#fff;;;https://sqlitecloud.io/]SQLite Cloud]',
+                '* [[!b;#fff;;;https://www.browserstack.com/]BrowserStack]',
+                ''
+            ].join('\n');
+        },
+        help() {
+            const list = formatter.format(command_list());
+            this.echo(`available commands: ${list}`, { keepWords: true });
+        }
+    };
+
+    const formatter = new Intl.ListFormat('en', {
+        style: 'long',
+        type: 'conjunction',
+    });
+
+    const term = $('body').terminal(commands, {
+        checkArity: false,
+        exit: false,
+        completion: true,
+        greetings: false,
+        onInit() {
+            this.echo(() => {
+                const cols = this.cols();
+                if (cols >= 92) {
+                    return greetings_big.innerHTML;
+                } else if (cols >= 57) {
+                    return greetings_medium.innerHTML;
+                } else if (cols >= 35) {
+                    return greetings_small.innerHTML;
+                } else {
+                    return greetings_tiny.innerHTML
+                }
+            });
+        }
+    });
+
+    term.echo('Type <command>jargon "hacker"</command> to get the entry from Jargon File.\nType <command>jargon -s hack</command> to search.\nType <command>help</command> to see other commands.\n');
+
+    term.on('click', 'a.jargon', function() {
+        const href = $(this).attr('href');
+        invoke(`jargon ${href}`);
+        return false;
+    }).on('click', 'a.command', function() {
+        const command = $(this).attr('href');
+        invoke(command);
+        return false;
+    }).on('click', 'a.rfc', function() {
+        const command = $(this).attr('href');
+        // are we inside RFC browser?
+        if (term.level() >= 2) {
+            commands.rfc(command);
+        } else {
+            invoke(`rfc ${command}`);
+        }
+        return false;
+    });
+
+    async function invoke(command) {
+        await term.exec(command);
+        if (term.settings().historyState) {
+            term.save_state(command);
+        }
+    }
+
+    function command_list() {
+        const list = Object.keys(commands);
+        list.push('clear');
+        return list.map(cmd => `<command>${cmd}</command>`);
+    }
+
+    function print_error(err) {
+        if (err.error) {
+            term.error(err.error.message);
+            term.error('in ' + err.error.file + ' at line ' +
+                                 err.error.at);
+            term.error(err.error.line);
+        } else {
+            term.error(err.message);
+        }
+    }
+});
+
+$.terminal.defaults.formatters.unshift([/(rfc\s?([0-9]+))/gi, '<rfc num="$2">$1</rfc>', {
+    echo: true
+}]);
+
+//$.terminal.defaults.formatters.push([/ ([0-9+]\. )/g, '\n$1']);
+$.terminal.xml_formatter.tags.name = () => '[[!bu;#fff;;jargon]';
+$.terminal.xml_formatter.tags.emphasis = () => '[[b;#fff;]';
+$.terminal.xml_formatter.tags.command = () => '[[!bu;#fff;;command]';
+$.terminal.xml_formatter.tags.rfc = ({ num }) => `[[!bu;yellow;;rfc;${num}]`;
+
+
+
+
+const BASE = 'https://cuho3e4lik.sqlite.cloud:8090';
+
+async function post(endpoint, data) {
+    const res = await fetch(`${BASE}/v2/functions/${endpoint}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ...data,
+            apikey: 'SBmxqjlBbI1Q0xlz3xr9vMNHBOewByrbAsEGYmfxlKE'
+        })
+    });
+    return res.json();
+}
+
+function jargon_term(query) {
+    return post('jargon-term', { query });
+}
+
+function jargon_search(query) {
+    return post('jargon-search', { query });
+}
+
+function format_entry(entries) {
+    let result = entries.map(function(entry) {
+        let text = '[[b;#fff;]' + entry.term + ']';
+        if (entry.abbrev) {
+            text += ' (' + entry.abbrev.join(', ') + ')';
+        }
+        let re = new RegExp("((?:https?|ftps?)://\\S+)|\\.(?!\\s|\\]\\s)\\)?", "g");
+        let def = entry.def.replace(re, function(text, g) {
+            return g ? g : (text == '.)' ? '.) ' : '. ');
+        });
+        return text + '\n' + def + '\n';
+    }).join('\n');
+    result = $.terminal.format_split(result).map(function(str) {
+        if ($.terminal.is_formatting(str)) {
+            return str.replace(/^\[\[([bu]{2};)/, '[[!$1');
+        }
+        return str;
+    }).join('');
+
+    return result;
+}
